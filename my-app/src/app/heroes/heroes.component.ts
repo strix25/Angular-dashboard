7e5eeda1f2af80df8322129324cb5c706b85993f
strix25 <damjan.oslaj@gmail.com>
2018-01-04 23:56:08 +0100

    .
    .

--- my-app/src/app/heroes/heroes.component.ts
+++ my-app/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,7 +10,8 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes: Hero[];
+  
 
   selectedHero: Hero;
 
