0c5f056dec8b9e28d9bda98f907384920c421805
strix25 <damjan.oslaj@gmail.com>
2018-01-06 23:16:05 +0100

    HTTP
    .

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 @NgModule({
   declarations: [
